6fc32b6271841bddce1e8008e0ad68b553764397
Shun Suzuki <suzuki@hapis.k.u-tokyo.ac.jp>
2019-08-18 17:41:02 +0900

    rename: force

---
+++ ps4000lib.example/Program.cs
@@ -0,0 +1,13 @@
+﻿using ps4000lib.example.Sample;
+
+namespace ps4000lib.example
+{
+    class Program
+    {
+        static void Main()
+        {
+            BlockImmediateSample.Run();
+            BlockTriggeredSample.Run();
+        }
+    }
+}
---
+++ ps4000lib.example/Sample/BlockTriggeredSample.cs
@@ -0,0 +1,37 @@
+﻿using PS4000Lib;
+using System;
+
+namespace ps4000lib.example.Sample
+{
+    public class BlockTriggeredSample
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Collect BlockData Triggered.");
+
+            using (var ps4000 = new PS4000())
+            {
+                ps4000.Open();
+
+                ps4000.ChannelA.Range = Range.Range_5V;
+                ps4000.ChannelB.Enabled = false;
+                ps4000.SamplingRateHz = 10_000;
+                ps4000.BufferSize = 10;
+
+                BlockData.Delimiter = ",";
+                BlockData.IgnoreHeader = true;
+
+                ps4000.ChannelA.TriggerVoltageMV = 500;
+                ps4000.ChannelA.TriggerMode = ThresholdMode.Level;
+                ps4000.ChannelA.TriggerDirection = ThresholdDirection.Rising;
+                ps4000.AddTriggerConditions(new TriggerConditions
+                {
+                    ChannelA = TriggerState.True,
+                });
+
+                var blockdata = ps4000.CollectBlockTriggered();
+                Console.WriteLine(blockdata);
+            }
+        }
+    }
+}
